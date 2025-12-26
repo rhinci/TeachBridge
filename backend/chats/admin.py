ffe6821b5814866058313efc7c2f45172d7dfe3a
rhinci <dashalupanova@gmail.com>
2025-12-26 23:56:14 +1000

    описаны модели users и chats + админки к ним

--- backend/chats/admin.py
+++ backend/chats/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Chat
 
-# Register your models here.
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'created_by', 'created_at')
+    list_filter = ('department', 'created_at')
+    filter_horizontal = ('teachers', 'study_groups')
+    search_fields = ('name', 'description')
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
